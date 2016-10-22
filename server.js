da408e3e13bf206d73f68a128c715204c6cb0b39
sagarwtca <sagar.wtca5@gmail.com>
2016-10-22 22:11:54 +0530

    add procfile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var port =Number(process.env.port||3000 );
 var path = require( 'path' );
 app.use(bodyParser.json());
 var passport = require('passport');
@@ -20,5 +21,5 @@ app.get('/', function(req, res) {
 })
 app.use(express.static('public'));
 app.listen(3000,function(err){
-console.log("server started");
+console.log("server started at",port);
 });
